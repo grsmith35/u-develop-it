4e0fb17d79c61e416746513395cca03dbbf5183c
Riley Smith <gordonrileysmith@gmail.com>
2021-10-07 10:52:09 -0600

    stuff

--- server.js
+++ server.js
@@ -1,25 +1,12 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const mysql = require('mysql2');
 const inputCheck = require('./utils/inputCheck');
+const db = require('./db/connection');
 
 //express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-//connect to database
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        //your mysql username
-        user: 'root',
-        password: 'G3tfucked1!',
-        database: 'election'
-    },
-    console.log('Connected to the election database.')
-);
-
 
 //get all candidates
 app.get('/api/candidate', (req, res) => {
